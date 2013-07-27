91a7a971f088c9421a62755982162c1f446507e3
Ubuntu <ubuntu@ip-172-31-18-65.us-west-2.compute.internal>
2013-07-27 15:09:18 +0000

    3784

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var express = require('express');
-// var fs = require('fs');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 console.log('1');
-//var buffer = fs.readFileSync("index.html");
+var buffer = fs.readFileSync("index.html");
 console.log('2');
-//var st2 = buffer.toString("utf-8");
-var st2="moshe";
+var st2 = buffer.toString("utf-8");
+//var st2="moshe";
 console.log('3');
 app.get('/', function(request, response) {
   response.send(st2);
